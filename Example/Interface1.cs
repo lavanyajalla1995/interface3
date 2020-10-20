8a96985e4e0219fe581ffe84c71b4f038b44d523
lavanyajalla1995 <jallalavanya1995@gmail.com>
2020-10-20 19:46:57 +0530

    Add project files.

---
+++ Example/Interface1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+    interface Interface1
+    {
+        void add();
+    }
+
+    interface Interface2
+    {
+        void add();
+    }
+
+    class A:Interface1,Interface2
+    {
+        public void add()
+        {
+            Console.WriteLine(2+6);
+        }
+        
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A og = new A();
+            og.add();
+            Console.Read();
+        }
+       
+        
+    }
+}
